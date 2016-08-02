98a832a9c7eb110436e0b4eab76abae4764cd738
feixue <2604671822@qq.com>
2016-08-02 23:11:44 +0800

    08

---
+++ 08/js/list.js
@@ -0,0 +1,3 @@
+var html="";
+html='<li class="goods">'+'<a href="">'+'<div class="images">'+'<img src="images/'+1+'">'+'</div>'+'<ul><li class="name"><a href="">商品名称</a></li><li class="price"><a href="">商品价格</a></li></ul>'+'</a>'+'</li>';
+		console.log(html);
